71470cc6fe7cbce03c5ebedba2836f4f32c5c9bf
최현준 <choijjunii1@gmail.com>
2025-03-06 16:41:56 +0900

    json데이터 요청

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from 'express'
-
+import fs from 'fs';
 // 원래방식대로 하면 됬는데 안돼서 __dirname사용
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -17,8 +17,13 @@ const PORT = 3000;
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'index.html'));
 })
-
-
+app.get('/champ', (req, res) => {
+  const champJson = fs.readFileSync(path.join(__dirname,'champion.json'),'utf-8');
+  const champion = JSON.parse(champJson);
+  console.log(champion);
+  res.json(champion);
+  })
+  
 
 app.listen(PORT, () => {
   console.log(`loading... http://localhost:${PORT}`);
