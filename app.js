7d5fe051d0eeb934a988b15ec322996aef86fc02
최현준 <choijjunii1@gmail.com>
2025-03-06 15:49:04 +0900

    서버생성 with express 완료

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// console.log("프로젝트시작")
+import express from 'express'
+
+// 원래방식대로 하면 됬는데 안돼서 __dirname사용
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+// public 정적파일 사용하기위해 static사용
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/public', express.static('public'));
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'index.html'));
+})
+
+
+
+app.listen(PORT, () => {
+  console.log(`loading... http://localhost:${PORT}`);
+})
