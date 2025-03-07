cbf3104af557aad41a0351ad397c6d4267cebe9e
김재승 <jaes@gmail.com>
2025-03-07 12:59:49 +0900

    벤픽로직구현완료

--- public/js/champDom.js
+++ public/js/champDom.js
@@ -2,10 +2,10 @@ import banPickLogic from "./championCheck.js";
 
 function championCreate(data) {
   let championList = document.getElementById("championList");
-  console.log(data);
+  // console.log(data);
   const champions = Object.values(data.data);
   champions.forEach((element) => {
-    console.log(element);
+    // console.log(element);
     championList.innerHTML += `<div><img src="./public/img/champion/${element.image.full}" alt=""></div>`;
   });
   const createdElements = championList.querySelectorAll("div");
--- public/js/championCheck.js
+++ public/js/championCheck.js
@@ -1,40 +1,42 @@
 // let championCheck = {};
 
 let championblue = document.getElementById("championblue"); //* 블루팀 벤 픽 이미지
-let championred = document.getElementById("championred"); //* 레드팀 벤 픽 이미지
+let championred = document.getElementById("championRed"); //* 레드팀 벤 픽 이미지
 let blueBtn = document.getElementById("blueBanBtn"); //* 블루팀 버튼
 let redBtn = document.getElementById("redBanBtn"); //* 레드팀 버튼
-
 let main = document.querySelectorAll(".championMainWrap > div"); //* 벤 할 챔피언
 let blueImgIndex = 0; //* 블루팀 벤픽 카운트
 let redImgIndex = 0; //* 레드팀 벤픽 카운트
 
 function imgChange(color) {
+  let allChecked = Array.from(
+    document.querySelectorAll(".champion-check input")
+  ).every((input) => input.checked); //* 모든 input이 checked인지 확인 (전역변수로 두면 체크된지 안된지 확인이 안됨)
   //* 블루팀 벤픽 로직
-  if (blueImgIndex < 5 && color === "blue") {
-    blueImgIndex++;
-    blueBtn.style.display = "none";
-    redBtn.style.display = "block";
-    console.log("blue" + blueImgIndex);
-    // } else {
-    //   blueImgIndex = 0;
-  }
-  //* 레드팀 벤픽 로직
-  if (redImgIndex < 5 && color === "red") {
-    redImgIndex++;
-    blueBtn.style.display = "block";
-    redBtn.style.display = "none";
-    console.log("red" + redImgIndex);
-    // } else {
-    //   redImgIndex = 0;
-  }
-  //* 블루팀 벤픽 카운트 초기화
-  if (blueImgIndex === 5 && color === "blue") {
-    blueImgIndex = 0;
-  }
-  //* 레드팀 벤픽 카운트 초기화
-  if (redImgIndex === 5 && color === "blue") {
-    redImgIndex = 0;
+  if (allChecked) {
+    if (blueImgIndex < 5 && color === "blue") {
+      blueImgIndex++;
+      blueBtn.style.display = "none";
+      redBtn.style.display = "block";
+      console.log("blue" + blueImgIndex);
+    }
+    //* 레드팀 벤픽 로직
+    if (redImgIndex < 5 && color === "red") {
+      redImgIndex++;
+      blueBtn.style.display = "block";
+      redBtn.style.display = "none";
+      console.log("red" + redImgIndex);
+    }
+    //* 블루팀 벤픽 카운트 초기화
+    if (blueImgIndex === 5 && color === "blue") {
+      blueImgIndex = 0;
+    }
+    //* 레드팀 벤픽 카운트 초기화
+    if (redImgIndex === 5 && color === "blue") {
+      redImgIndex = 0;
+    }
+  } else {
+    alert("챔피언을 선택해주세요.");
   }
 
   // console.log(blueImgIndex);
@@ -42,48 +44,29 @@ function imgChange(color) {
 function banPickLogic(elements) {
   elements.forEach((elementdata) => {
     elementdata.addEventListener("click", () => {
-      let mainImg = elementdata.querySelector("img");
-      let blueImgs = championblue.querySelectorAll("img");
-      let blueImg = blueImgs[blueImgIndex];
-      blueImg.src = mainImg.src;
-      blueImgIndex = (blueImgIndex + 1) % blueImgs.length;
+      let allChecked = Array.from(
+        document.querySelectorAll(".champion-check input")
+      ).every((input) => input.checked); //* 모든 input이 checked인지 확인 (전역변수로 두면 체크된지 안된지 확인이 안됨)
+      if (allChecked) {
+        if (blueBtn.style.display === "block") {
+          let mainImg = elementdata.querySelector("img");
+          let blueImgs = championblue.querySelectorAll("img");
+          let blueImg = blueImgs[blueImgIndex];
+          blueImg.src = mainImg.src;
+        } else if (redBtn.style.display === "block") {
+          let mainImg = elementdata.querySelector("img");
+          let redImgs = championred.querySelectorAll("img");
+          let redImg = redImgs[redImgIndex];
+          redImg.src = mainImg.src;
+        }
+      } else {
+        alert("챔피언을 선택해주세요.");
+      }
     });
   });
 }
-// console.log(main);
-// function banPickLogic(element) {
-//   console.log(element);
-//   element.forEach((elementdata) => {
-//     elementdata.addEventListener("click", () => {
-//       // if (mainImg) {
-//       let blueImgs = championblue.querySelectorAll("img");
-//       // if (blueImgs.length > 0) {
-//       let blueImg = blueImgs[blueImgIndex];
-//       // if (blueImg) {
-//       blueImg.src = mainImg.src;
-//       // blueImgIndex = blueImgIndex % blueImgs.length;
-//       // }
-//       // }
-//       // }
-//     });
-//   });
-// }
+// elementdata.style.pointerEvents = "none"; 클릭 못하게 막는 이벤트
+// elementdata.style.opacity = "0.3";
 window.imgChange = imgChange;
 
 export default banPickLogic;
-// main.forEach((mainBtn) => {
-//   mainBtn.addEventListener("click", () => {
-//     let mainImg = mainBtn.querySelector("img");
-//     console.log(mainImg);
-//     // if (mainImg) {
-//     let blueImgs = championblue.querySelectorAll("img");
-//     // if (blueImgs.length > 0) {
-//     let blueImg = blueImgs[blueImgIndex];
-//     // if (blueImg) {
-//     blueImg.src = mainImg.src;
-//     // blueImgIndex = blueImgIndex % blueImgs.length;
-//     // }
-//     // }
-//     // }
-//   });
-// });
