312c1c615374485f0800b79c2866025ac83759ce
최현준 <choijjunii1@gmail.com>
2025-03-07 13:02:11 +0900

    검색기능 구현중

--- public/js/champDom.js
+++ public/js/champDom.js
@@ -1,16 +1,53 @@
+// function championCreate(data) {
+//   let championList = document.getElementById("championList");
+//   console.log(data);
+//   const champions = Object.values(data.data);
+    // sort메서드로 정렬해봄.
+//     champions.sort((a, b) => a.name.localeCompare(b.name, "ko-KR"));
+
+//   champions.forEach((element) => {
+//     console.log(element.image);
+    // p태그 생성하여 챔피언 이름 작성. css도 수정완료 확인바라고 수정할 부분있으면 최현준에게 말해주세요 //
+//     championList.innerHTML += `<div><img src="./public/img/champion/${element.image.full}" alt="${element.name}"><p>${element.name}</p></div>`;
+//   });
+// }
+
+// export default championCreate;
+
+
 function championCreate(data) {
   let championList = document.getElementById("championList");
+  let searchBox = document.getElementById("searchBox"); // 검색 입력 필드
   console.log(data);
-  const champions = Object.values(data.data);
-    // sort메서드로 정렬해봄.
-    champions.sort((a, b) => a.name.localeCompare(b.name, "ko-KR"));
 
-  champions.forEach((element) => {
-    console.log(element.image);
-    // p태그 생성하여 챔피언 이름 작성. css도 수정완료 확인바라고 수정할 부분있으면 최현준에게 말해주세요 //
-    championList.innerHTML += `<div><img src="./public/img/champion/${element.image.full}" alt="${element.name}"><p>${element.name}</p></div>`;
+  const champions = Object.values(data.data);
+  // sort메서드로 정렬해봄.
+  champions.sort((a, b) => a.name.localeCompare(b.name, "ko-KR"));
+
+  // 챔피언 목록을 출력.
+  function displayChampions(championArray) {
+    championList.innerHTML = ""; // 기존 목록 초기화
+    championArray.forEach((element) => {
+      // p태그 생성하여 챔피언 이름 작성. css도 수정완료 확인바라고 수정할 부분있으면 최현준에게 말해주세요 //
+      championList.innerHTML += `
+        <div>
+          <img src="./public/img/champion/${element.image.full}" alt="${element.name}">
+          <p>${element.name}</p>
+        </div>
+      `;
+    });
+  }
+
+  // 검색 기능 추가함.
+  searchBox.addEventListener("input", function () {
+    const searchText = searchBox.value.toLowerCase(); 
+    const filteredChampions = champions.filter((data) =>
+      data.name.toLowerCase().includes(searchText) 
+    );
+    displayChampions(filteredChampions); 
   });
+
+  displayChampions(champions);
 }
 
 export default championCreate;
-
