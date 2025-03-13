a50f3c6df71fa2d3741f4ee28d1bc6914581792d
최정민 <bigdragon00@naver.com>
2025-03-13 15:37:40 +0900

    그릇 만듬

---
+++ public/js/chamPick_object.js
@@ -0,0 +1,14 @@
+let championblue = document.getElementById("championblue"); // 블루팀 벤 픽 이미지
+let championred = document.getElementById("championRed"); // 레드팀 벤 픽 이미지
+let blueTeam = document.getElementById("blueTeam"); // 블루팀 챔피언 선택
+let redTeam = document.getElementById("redTeam"); // 레드팀 챔피언 선택
+let blueBtn = document.getElementById("blueBanBtn"); // 블루팀 버튼
+let redBtn = document.getElementById("redBanBtn"); // 레드팀 버튼
+let clickLabel = Array.from(document.querySelectorAll(".champion-check input")); // 체크박스 배열
+let blueImgIndex = 0; // 블루팀 벤픽 카운트
+let redImgIndex = 0; // 레드팀 벤픽 카운트
+let article = document.querySelectorAll("article label"); // 모든 label 요소 선택
+
+
+let champPickObj = { championblue, championred, blueTeam, redTeam, blueBtn, redBtn, clickLabel, blueImgIndex, redImgIndex, article };
+console.log(champPickObj);
